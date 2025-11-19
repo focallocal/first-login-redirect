70c446a3ba33043a29f7bdd47a2b87461da1f04f
Public Happiness Andy <focallocal.london@gmail.com>
2025-11-19 20:57:39 +0000

    Simplify: Use single above-main-container outlet like category-headers

--- javascripts/discourse/api-initializers/groups-welcome-banner.js
+++ javascripts/discourse/api-initializers/groups-welcome-banner.js
@@ -2,21 +2,6 @@ import { apiInitializer } from "discourse/lib/api";
 import GroupsWelcomeBanner from "../components/groups-welcome-banner";
 
 export default apiInitializer("1.8.0", (api) => {
-  console.log("📢 Groups Welcome Banner initializer loading");
-  
-  // Try multiple possible outlets for the groups page
-  const outlets = [
-    "above-main-container",
-    "discovery-list-container-top",
-    "before-list-area",
-    "groups-header",
-    "before-group-members"
-  ];
-  
-  outlets.forEach(outlet => {
-    console.log("🔌 Attempting to render banner in outlet:", outlet);
-    api.renderInOutlet(outlet, GroupsWelcomeBanner);
-  });
-  
-  console.log("✅ Banner registered for", outlets.length, "outlets");
+  console.log("📢 Groups Welcome Banner: Rendering in above-main-container");
+  api.renderInOutlet("above-main-container", GroupsWelcomeBanner);
 });
